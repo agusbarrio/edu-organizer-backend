fe42ddf3e185c817b108ff9ed0d6475acc79729f
Agustin Barrio <agustin.b@solunika.com>
2023-07-02 22:57:46 -0300

    change for redeploy

--- models/course.js
+++ models/course.js
@@ -33,7 +33,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       name: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: false
       },
       shortId: {
         type: DataTypes.STRING(5),
