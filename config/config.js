9aaf9021931b75b20e3825b040175d3a88e5d80f
Agustin Barrio <agustin.b@solunika.com>
2023-08-03 23:36:58 -0300

    delete console log

--- config/config.js
+++ config/config.js
@@ -3,8 +3,6 @@
 const path = !!process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
 require('dotenv').config({ path });
 
-console.log('MYSQL_CONNECTION', process.env.MYSQL_CONNECTION)
-
 module.exports = {
   use_env_variable: 'MYSQL_CONNECTION',
 };
