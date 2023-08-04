5b39cf2974fb0f3dba455dc58dbafb6cb68bb5fb
Agustin Barrio <agustin.b@solunika.com>
2023-08-03 23:07:09 -0300

    test

--- config/config.js
+++ config/config.js
@@ -3,6 +3,8 @@
 const path = !!process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
 require('dotenv').config({ path });
 
+console.log('MYSQL_CONNECTION', process.env.MYSQL_CONNECTION)
+
 module.exports = {
   use_env_variable: 'MYSQL_CONNECTION',
 };
